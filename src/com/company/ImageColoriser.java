602fc87d02d95f47cfb5174a7ee5a907c68f06eb
alu10186575 <aluded6575@ieselcaminas.org>
2022-05-09 20:48:21 +0200

    Initial commit

---
+++ src/com/company/ImageColoriser.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageColoriser {
+    public void redImageColoriser() throws IOException {
+        File file = (new File("./image/tillindeman.bmp"));
+        BufferedImage img = ImageIO.read(file);
+
+        for (int x= 0;x<img.getWidth();x++){
+            for (int y=0; y<img.getHeight();y++){
+
+                int pixel= img.getRGB(x,y);
+
+                Color color = new Color (pixel,true);
+
+                int red = color.getRed();
+                int green= color.getGreen();
+                int blue = color.getBlue();
+
+                green= 0;
+                blue =0;
+
+                color = new Color(red,green,blue);
+                int newPixel =color.getRGB();
+                img.setRGB(x,y,newPixel);
+            }
+        }
+
+        file = new File("./image/tillindemanRed.bmp");
+        ImageIO.write(img,"bmp",file);
+        System.out.println("Till Lindeman Done...");
+    }
+}
